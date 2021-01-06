232bb7a11de6520db725e3317fed92e37094698c
aavetisyanit <aavetisyanit@gmail.com>
2021-01-05 20:43:06 -0600

    Create about, create, 404 routes

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ app.get('/', (req, res) => {
 	res.render('index');
 });
 
+app.get('/about', (req, res) => {
+	res.render('about');
+});
+app.get('/blogs/create', (req, res) => {
+	res.render('create');
+});
+
+//404 page
+app.use((req, res) => {
+	res.status(404).render('404');
+});
+
 app.listen(3000, () => {
 	console.log('Server is running on port 3000');
 });
