be9df87b9adf0e75f655a7cbd6e278d4d73d9d32
aavetisyanit <aavetisyanit@gmail.com>
2021-01-05 23:15:02 -0600

    Make connection to MongoDB. Install mongoose and morgan

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+//connect to mongodb
+const dbURI =
+	'mongodb+srv://admin:database1234@myblogs.tptfc.mongodb.net/<myBlogs>?retryWrites=true&w=majority';
+mongoose
+	.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then((result) => {
+		//first connect to DB
+		console.log('connected to db');
+		//then listen for requests
+		app.listen(3000, () => {
+			console.log('Server is running on port 3000');
+		});
+	})
+	.catch((err) => console.log(err));
 
 app.set('view engine', 'ejs');
+
+//middleware and static files
 app.use('/public', express.static('public'));
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
 	const blogs = [
@@ -32,8 +52,4 @@ app.get('/blogs/create', (req, res) => {
 //404 page
 app.use((req, res) => {
 	res.status(404).render('404', { title: '404' });
-});
-
-app.listen(3000, () => {
-	console.log('Server is running on port 3000');
 });
