859121e3e05e1a50fe24db7a125d871607b9e392
aavetisyanit <aavetisyanit@gmail.com>
2021-01-05 22:07:57 -0600

    Add styles

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
 	const blogs = [
