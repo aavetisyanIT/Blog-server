06896d1aa1dc20c1b8edbafb91eda9effab979a0
aavetisyanit <aavetisyanit@gmail.com>
2021-01-10 13:31:27 -0600

    Add delete icon

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -17,7 +17,9 @@ const blog_details = (req, res) => {
 		.then((result) => {
 			res.render('details', { title: 'Blog Details', blog: result });
 		})
-		.catch((err) => console.log(err));
+		.catch((err) => {
+			res.status(404).render('404', { title: 'Blog not found' });
+		});
 };
 
 //create blog page
